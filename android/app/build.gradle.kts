8c6863f4ebf8b984f78ea98dfc8cb84c1c0e059c
mukeshbala143 <mukeshbala95133@gmail.com>
2026-06-23 23:33:06 +0530

    change and add splash

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,59 +1,108 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     id("com.android.application")
+
     // START: FlutterFire Configuration
     id("com.google.gms.google-services")
     // END: FlutterFire Configuration
+
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+
+    // Flutter plugin always last
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+
 android {
+
     namespace = "com.example.cartkaro"
+
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+
+    ndkVersion = "28.2.13676358"
+
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+
+        // Required for flutter_local_notifications
+        isCoreLibraryDesugaringEnabled = true
     }
 
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
-    }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+
         applicationId = "com.example.cartkaro"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+
         minSdk = flutter.minSdkVersion
+
         targetSdk = flutter.targetSdkVersion
+
         versionCode = flutter.versionCode
+
         versionName = flutter.versionName
     }
 
+
     buildTypes {
+
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
+
+            // Debug signing for testing
             signingConfig = signingConfigs.getByName("debug")
         }
     }
 }
 
+
+// Kotlin 2.3+ compiler configuration
+
+kotlin {
+
+    compilerOptions {
+
+        jvmTarget.set(JvmTarget.JVM_17)
+    }
+}
+
+
+
 flutter {
+
     source = "../.."
 }
 
-dependencies {
-  implementation(kotlin("stdlib-jdk8"))
-  // Import the Firebase BoM
-  implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
 
-  // TODO: Add the dependencies for Firebase products you want to use
-  // When using the BoM, don't specify versions in Firebase dependencies
-  implementation("com.google.firebase:firebase-analytics")
-  implementation("com.google.firebase:firebase-auth")
-  implementation("com.google.firebase:firebase-firestore")
+
+dependencies {
+
+
+    // Kotlin
+    implementation(kotlin("stdlib-jdk8"))
+
+
+
+    // Core library desugaring
+    coreLibraryDesugaring(
+        "com.android.tools:desugar_jdk_libs:2.1.5"
+    )
+
+
+
+    // Firebase BoM
+    implementation(
+        platform("com.google.firebase:firebase-bom:33.1.0")
+    )
+
+
+
+    // Firebase services
+    implementation("com.google.firebase:firebase-analytics")
+
+    implementation("com.google.firebase:firebase-auth")
+
+    implementation("com.google.firebase:firebase-firestore")
 }
